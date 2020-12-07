7650b1b4d7b9675cc5bd73bcbadc9bd2998e320e
spaceC00kie <kirstie317@gmail.com>
2020-12-06 23:03:59 -0500

    more of the video

---
+++ traversyjs05.js
@@ -0,0 +1,13 @@
+const s = "Hello World";
+
+console.log(s.length);
+
+console.log(s.toUpperCase());
+
+console.log(s.toLowerCase());
+
+console.log(s.substring(0, 5));
+
+console.log(s.substring(0, 5).toUpperCase());
+
+console.log(s.split(""));
